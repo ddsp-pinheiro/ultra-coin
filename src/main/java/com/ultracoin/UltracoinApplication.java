3205ef61bf8eab2f04924dc9a5f60da33ebe548a
Daniele Pinheiro <ddsp.pinheiro@gmail.com>
2022-09-07 02:54:29 -0300

    Projeto base

---
+++ src/main/java/com/ultracoin/UltracoinApplication.java
@@ -0,0 +1,13 @@
+package com.ultracoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltracoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltracoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ultracoin/UltracoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultracoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltracoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

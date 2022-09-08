0c2b8dfc6a81ab9f97582b6c29451e0964acfbef
Daniele Pinheiro <ddsp.pinheiro@gmail.com>
2022-09-08 13:23:35 -0300

    Add interface repositoryPerson com findById

---
+++ src/main/java/com/ultracoin/repository/PersonRepository.java
@@ -0,0 +1,11 @@
+package com.ultracoin.repository;
+
+import com.ultracoin.entity.PersonEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
+    Optional<PersonEntity> findById(Long id);
+}

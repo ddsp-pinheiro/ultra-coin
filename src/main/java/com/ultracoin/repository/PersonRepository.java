5770a378beeae9b8a4261f024f21385b8edfe30a
Daniele Pinheiro <ddsp.pinheiro@gmail.com>
2022-09-09 18:09:04 -0300

    Add consulta banco de dados

--- src/main/java/com/ultracoin/repository/PersonRepository.java
+++ src/main/java/com/ultracoin/repository/PersonRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 @Repository
 public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
     Optional<PersonEntity> findById(Long id);
+    Optional<PersonEntity> findByTaxId(Long taxId);
 }

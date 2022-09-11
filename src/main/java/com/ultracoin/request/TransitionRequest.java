d1b22c3fdc3b7d9ca665452dbd5b54e48840e8e7
Daniele Pinheiro <ddsp.pinheiro@gmail.com>
2022-09-10 23:21:00 -0300

    Alterando request transition

--- src/main/java/com/ultracoin/request/TransitionRequest.java
+++ src/main/java/com/ultracoin/request/TransitionRequest.java
@@ -3,11 +3,6 @@ package com.ultracoin.request;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
